bf7c3fc127f054be666181f2f53ded28b51f695d
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-02 17:38:30 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown(number)
+  number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-    return "HAPPY NEW YEAR!" if count == 12
+  "HAPPY NEW YEAR!"
 end
