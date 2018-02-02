5a1a14cd337dc970c334d170cea95de59ae48b22
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-02 17:34:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 
 def countdown(n)
   counter = 10
-  while n > 1
-    counter -= 1
-    10.times do
+  while n > 0
     puts "#{n} SECOND(S)!"
+    n -= 1
+    count += 1
   end
     return "HAPPY NEW YEAR!"
 end
